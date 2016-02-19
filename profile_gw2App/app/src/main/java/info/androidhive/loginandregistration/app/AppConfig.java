e67330d00e192833912861580409a547101e3523
Krijn van der Burg <krijnc@hotmail.com>
2016-02-19 21:09:07 +0100

    Resetting from laptop after error

--- profile_gw2App/app/src/main/java/info/androidhive/loginandregistration/app/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package info.androidhive.loginandregistration.app;
-
-public class AppConfig {
-	// Server user login url
-	public static String URL_LOGIN = "http://192.168.1.12:1337/profile_gw2Authentication/login.php";
-
-	// Server user register url
-	public static String URL_REGISTER = "http://192.168.1.12:1337/profile_gw2Authentication/register.php";
-}
