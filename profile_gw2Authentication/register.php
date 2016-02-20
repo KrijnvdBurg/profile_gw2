f39f7afffe7b09c033429c72c4abcd13264902a9
Krijn van der Burg <krijnc@hotmail.com>
2016-02-20 12:22:56 +0100

    Added api_key to PHP authentication

--- profile_gw2Authentication/register.php
+++ profile_gw2Authentication/register.php
@@ -16,6 +16,7 @@ if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password'])
     // receiving the post params
     $name = $_POST['name'];
     $email = $_POST['email'];
+    $api_key = $_POST['api_key'];
     $password = $_POST['password'];
 
     // check if user is already existed with the same email
@@ -26,13 +27,14 @@ if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password'])
         echo json_encode($response);
     } else {
         // create a new user
-        $user = $db->storeUser($name, $email, $password);
+        $user = $db->storeUser($name, $email, $api_key, $password);
         if ($user) {
             // user stored successfully
             $response["error"] = FALSE;
             $response["uid"] = $user["unique_id"];
             $response["user"]["name"] = $user["name"];
             $response["user"]["email"] = $user["email"];
+            $response['user']['api_key'] = $user['api_key'];
             $response["user"]["created_at"] = $user["created_at"];
             $response["user"]["updated_at"] = $user["updated_at"];
             echo json_encode($response);
@@ -45,7 +47,7 @@ if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password'])
     }
 } else {
     $response["error"] = TRUE;
-    $response["error_msg"] = "Required parameters (name, email or password) is missing!";
+    $response["error_msg"] = "Required parameters (name, email, api key or password) is missing!";
     echo json_encode($response);
 }
 ?>
